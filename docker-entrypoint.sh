3a310aefb632fb2f6902db64e96bb89224f37932
sokhnaawa19 <mawagashou@gmail.com>
2026-09-04 11:55:15 +0000

    render test2

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,9 @@ composer install --no-dev --optimize-autoloader --no-interaction
 echo "Migration de la base de données..."
 php artisan migrate --force
 
+echo "Création du lien de stockage public..."
+php artisan storage:link || true
+
 echo "Remplissage des données de démo (seeders)..."
 php artisan db:seed --force
 
